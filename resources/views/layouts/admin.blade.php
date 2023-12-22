e1b0a3f0ec823d6b6b7501ad070d10227d66a354
Toghul Guluzadeh <toghrulvg@code.edu.az>
2023-12-22 16:58:10 +0400

    added

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- plugins:css -->
+  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/favicon.png">
+
+  <link href="assets\css\bootstrap.min.css" rel="stylesheet">
+
+
+    @livewireStyles
+</head>
+<body>
+
+    <div class="container-scroller">
+        @include('layouts.inc.admin.navbar')
+        <div class="container-fluid page-body-wrapper">
+            @include('layouts.inc.admin.sidebar')
+
+
+            <div class="main-panel">
+                <div class="content-warpper">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{asset('admin/vendors/base/vendor.bundle.base.js')}}"></script>
+    <script src="{{asset('admin/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{asset('admin/js/off-canvas.js')}}"></script>
+    <script src="{{asset('admin/js/hoverable-collapse.js')}}"></script>
+    <script src="{{asset('admin/js/template.js')}}"></script>
+    <script src="{{asset('admin/js/dashboard.js')}}"></script>
+    <script src="{{asset('admin/js/data-table.js')}}"></script>
+    <script src="{{asset('admin/js/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('admin/js/dataTables.bootstrap4.js')}}"></script>
+
+
+    @livewireScripts
+</body>
+</html>
